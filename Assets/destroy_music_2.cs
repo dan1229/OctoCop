ee360dc01d601fa13da5dd14ef2d26ec74ebb25f
Jose Torres <charpie@ufl.edu>
2018-04-20 03:04:52 -0700

    fixed music

---
+++ Assets/destroy_music_2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy_music_2: MonoBehaviour
+{
+
+    // Use this for initialization
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music_lvl1");
+        if (objs.Length > 0)
+            Destroy(objs[0]);
+    }
+}
---
+++ Assets/destroy_music_3.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy_music_3 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music_lvl2");
+        if (objs.Length > 0)
+            Destroy(objs[0]);
+    }
+}
